50ab4198d5ea379d3f9c9417bebe1e7195d5915c
Angela K Venable <avenable11@ivytech.edu>
2021-09-08 21:04:47 -0400

    State after 9/8/21 class

--- app/src/main/java/edu/ivytech/tipcalculatorfall2021/MainActivity.kt
+++ app/src/main/java/edu/ivytech/tipcalculatorfall2021/MainActivity.kt
@@ -2,10 +2,60 @@ package edu.ivytech.tipcalculatorfall2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+import edu.ivytech.tipcalculatorfall2021.databinding.ActivityMainBinding
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var tipPercent = .15
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.upButton.setOnClickListener {
+            tipPercent += .01
+            displayPercent()
+            //binding.upButton.isEnabled = tipPercent <= .30
+        }
+        binding.downButton.setOnClickListener {
+            tipPercent -= .01
+            displayPercent()
+        }
+        binding.calculateBtn.setOnClickListener{
+            calculateAndDisplay()
+        }
     }
+
+    private fun displayPercent() {
+        val percentFormat = NumberFormat.getPercentInstance()
+        val symbol = DecimalFormatSymbols()
+        symbol.percent = ' '
+        (percentFormat as DecimalFormat).decimalFormatSymbols = symbol
+        binding.tipPercentEditText.setText(percentFormat.format(tipPercent))
+    }
+
+    private fun calculateAndDisplay() {
+        val billAmountStr = binding.billAmountEditText.text.toString()
+        var billAmount = 0.0
+        if(billAmountStr.isNotEmpty()) {
+            billAmount = billAmountStr.toDouble()
+        }
+        val tipAmount = billAmount * tipPercent
+        val totalAmount = billAmount + tipAmount
+
+        val currencyFormat = NumberFormat.getCurrencyInstance()
+        val symbol = DecimalFormatSymbols()
+        symbol.currencySymbol = ""
+        (currencyFormat as DecimalFormat).decimalFormatSymbols = symbol
+        binding.tipAmountEditText.setText(currencyFormat.format(tipAmount))
+        binding.totalAmountEditText.setText(currencyFormat.format(totalAmount))
+        binding.billAmountEditText.setText(currencyFormat.format(billAmount))
+        displayPercent()
+    }
+
 }
